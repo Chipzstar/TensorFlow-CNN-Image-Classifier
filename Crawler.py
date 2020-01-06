d863762befff5fc2b2022dc323ead4bc649abd77
Chisom Oguibe <chipzstar.dev@googlemail.com>
2020-01-06 19:42:52 +0000

    Optimized crawler
    - Can now select the number of images to download
    - Crawler now runs iteratively
    - Can now select previously entered model name for consecutive crawls

--- Crawler.py
+++ Crawler.py
@@ -3,10 +3,10 @@
 
 from bs4 import BeautifulSoup
 import requests as req
-import os
-import re
+import os, re
 import numpy as np
 
+#%%
 
 CHISOM_FILE_PATH = os.path.normpath('C:/Users/chiso/MEGA/data/new')
 YISI_FILE_PATH = os.path.normpath('E:/MegaSync/data/new')
@@ -37,8 +37,7 @@ def file_numbering(path):
             num_files = len(files)  # number of files in folder
             regex = re.compile(r'\d+')  # regular expression to extract only the number from filename(s)
             file_numbers = np.array([regex.findall(file) for file in files]).astype(int)  # store all file numbers in an array
-            number = max(file_numbers.flatten()) if file_numbers.flatten().size > 0 else 0
-            # number = max(file_numbers.flatten())  # grab the largest value in array
+            number = max(file_numbers.flatten()) if file_numbers.flatten().size > 0 else 0  # grab the largest value in array
             if number != num_files:  # compare the max value in array to num_files, if they are different then some images may be missing or deleted from folder
                 print("WARNING: files in this directory are numbered incorrectly!")
                 print(f"Number of files in folder: {num_files}")
@@ -49,32 +48,50 @@ def file_numbering(path):
     return number
 
 
+def check_prev_model_name():
+    if 'MODEL_NAME' in globals():  # Check if the variable 'MODEL NAME' exists in the program
+        return True
+    return False
+
+
+def get_model_name():
+    response = input(f"Use previous model name: {MODEL_NAME}? y/n")
+    if response == 'y' or response == 'Y':
+        return MODEL_NAME
+    return input("Enter name of new model: ")
+
+
 def find_list_items(soup2):
-    locator = "div#wrapper div#content div#main ul#tiles li.thumbwook a"  # CSS Selector
+    locator = "div#wrapper div#content div#main ul#tiles li.thumbwook a" # CSS Selector
 
-    # image = soup2.select_one(locator) # selects one item from the locator
-    # image_link = image.attrs['src'] # selects the 'src' attribute value
-
-    images = soup2.select(locator)  # selects all items found in the locator
-    image_links = [image.attrs['href'] for image in images]  # selects the 'href' attribute value
+    images = soup2.select(locator) # selects all items found in the locator
+    image_links = [image.attrs['href'] for image in images] # selects the 'href' attribute value
     return image_links
 
 
 def download_images(file_path, file_name):
     print(file_path)
-    r2 = req.get(URL)
-    soup2 = BeautifulSoup(r2.text, "html.parser")
+    r2 = req.get(URL)  # makes a http request to the web site url
+    soup2 = BeautifulSoup(r2.text, "html.parser")  # parses the website
     image_files = find_list_items(soup2)
     init_num = file_numbering(file_path)
     for index, img_link in enumerate(image_files):
+        if index == NUM_IMAGES:
+            break
         img_data = req.get(img_link).content
         with open(f"{file_path}/{file_name} ({str(init_num+index+1)}).jpg", 'wb+') as f:
             f.write(img_data)
             print(f"Image '{file_name}{init_num+index+1}' was successfully written!")
 
 
-URL = input("Enter the URL to download images from: ")
-MODEL_NAME = input("Enter the name of the model: ").title()
-FILE_NAME = MODEL_NAME.lower().split(" ")[0]
-FILE_PATH = get_file_path(MODEL_NAME)
-download_images(FILE_PATH, FILE_NAME)
+is_finished = False
+while not is_finished:
+    URL = input("Enter the URL to download images from: ")
+    MODEL_NAME = input("Enter the name of the model: ").title() if not check_prev_model_name() else get_model_name()
+    NUM_IMAGES = int(input("Number of images to select: "))
+    FILE_NAME = MODEL_NAME.lower().split(" ")[0]
+    FILE_PATH = get_file_path(MODEL_NAME)
+    download_images(FILE_PATH, FILE_NAME)
+    CONTINUE = input("\nContinue? y/n")
+    if not (CONTINUE == 'y' or CONTINUE == 'Y'):
+        is_finished = True
