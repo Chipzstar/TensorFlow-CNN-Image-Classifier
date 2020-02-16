36755cb1e891f35f6d0e570688ccdf4b909bf046
Chipzstar <chipzstar.dev@googlemail.com>
2020-02-16 14:04:01 +0000

    FIX: added files from prev commit

--- inference/inference.py
+++ inference/inference.py
@@ -1,42 +1,63 @@
+import io, re, json, boto3, base64, time, requests
+import numpy as np
+import PIL
+from PIL import Image
+
 JPEG_CONTENT_TYPE = 'image/jpeg'
 PNG_CONTENT_TYPE = 'image/png'
+IMG_SIZE = 100
+CLIENT = boto3.client('logs')
+LOG_GROUP_NAME='/aws/sagemaker/Endpoints/pornilarity-v1-endpoint'
+LOG_STREAM_NAME = f"[{time.strftime('%Y/%M/%d - %H/%M/%S')}] <inference_logs>"
+
+response = CLIENT.create_log_stream(
+    logGroupName=LOG_GROUP_NAME,
+    logStreamName=LOG_STREAM_NAME
+)
+
+def getToken():
+    response = CLIENT.describe_log_streams(
+        logGroupName=LOG_GROUP_NAME,
+        logStreamNamePrefix=LOG_STREAM_NAME,
+    )
+    try:
+        token = response['logStreams'][0]['uploadSequenceToken']
+    except:
+        token = None
+    return token
        
+def get_current_timestamp():
+    timestamp = int(time.time()*1000)
+    return timestamp
+    
+    
 def get_payload(event):
     data = json.loads(json.dumps(event))
     payload =  data['instances']
     return payload
 
 
-def process_image(image_file):
-    np_array = np.fromstring(image_file, np.uint8)
-    image_tensor = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
-    gray = cv2.cvtColor(image_tensor, cv2.COLOR_BGR2GRAY) # grayscale the image
-    image = cv2.resize(gray, (IMG_SIZE, IMG_SIZE)) # resize, as our model is expecting images in 32x32.
+def process_image_pillow(image_data):
+    image_content = image_data.read()
+    dataBytesIO = io.BytesIO(image_content)
+    pillow_array = np.frombuffer(dataBytesIO.read(), np.uint8) # reads in byte pixel data and converts to numeric numpy array
+    image = Image.open(dataBytesIO) # converts pixel data into Pillow image format
+    gray = image.convert('L') # grayscales the image (removes RGB channels)
+    image = gray.resize((IMG_SIZE, IMG_SIZE)) # resizes image to correct image size
     image = np.array(image).reshape(-1, IMG_SIZE, IMG_SIZE, 1) # reshapes the image into accepted dimensions (4D tensor)
-    image = image.astype('float32') # converts datatype to 32-bit float
+    image = image.astype('float32') # change data type of image to float32
     image = image / 255.0 # Normalisation of RGB pixel values
     return image
     
     
-def convert_bytes_to_array(response_body):
+def convert_json_to_array(response_body):
     body = " ".join(response_body.split())
     regex = re.compile("(?![e])[a-z\"\}\{:\s\[\]]*")
     predictions = regex.sub("", body).split(',')
     predictions = np.array(predictions)
     predictions = predictions.astype(np.float64)
     return predictions
-    
-    
-def get_top5(predictions, labels):
-    top_5 = []
-    count = 0
-    while count < 5:
-        max_index = np.argmax(predictions)
-        class_label = labels[max_index]
-        top_5.append(class_label)
-        predictions = np.delete(predictions, max_index)
-        count+=1
-    return top_5
+
     
 def input_handler(data, context):
     """ Pre-process request input before it is sent to TensorFlow Serving REST API
@@ -49,13 +70,10 @@ def input_handler(data, context):
         (dict): a JSON-serializable dict that contains request body and headers
     """
     if context.request_content_type == 'application/x-image':
-        print(f"Base64 image: {data}")
-        payload = data.read()
-        print(f"Payload: {payload}")
-        # encoded_image = base64.b64encode(payload).decode('utf-8')
-        # print(f"Encoded Image: {encoded_image}")
-        # instance = [{"b64": encoded_image}]
-        return json.dumps({"instances": payload})
+        processed_image = process_image_pillow(data)
+        image_tensor = np.asarray(processed_image).tolist()
+        payload = {"instances": image_tensor}
+        return json.dumps(payload)
     else:
         _return_error(415, 'Unsupported content type "{}"'.format(context.request_content_type or 'Unknown'))
 
@@ -73,8 +91,22 @@ def output_handler(response, context):
     if response.status_code != 200:
         _return_error(response.status_code, response.content.decode('utf-8'))
     response_content_type = context.accept_header
-    prediction = response.content
-    return prediction, response_content_type
+    response_body = response.content.decode("utf-8")
+    predictions = convert_json_to_array(response_body)
+    predictions = predictions.tolist()
+    if not getToken():
+        response = CLIENT.put_log_events(
+           logGroupName=LOG_GROUP_NAME,
+           logStreamName=LOG_STREAM_NAME,
+           logEvents=[
+               {
+                   'timestamp': get_current_timestamp(),
+                   'message': response_body
+               },
+           ]
+        )
+    # result = dict({'predictions': predictions})
+    return json.dumps(predictions), response_content_type
 
 
 def _return_error(code, message):
